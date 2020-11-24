7266f92fd65ef751034b233b3b25a5343821a3c1
Gabriel THEUWS <Gabriel.THEUWS@etu.uca.fr>
2020-11-24 11:32:13 +0100

    [DEV] création d'un header et d'un footer pour factoristaion du code

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!-- Footer de ma page chargeant le javascript -->
+      </div>
+    </div>
+
+    <div class="fixed-bottom text-yellow" >Icons made by Freepik from www.flaticon.com</a></div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="bootstrap/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
