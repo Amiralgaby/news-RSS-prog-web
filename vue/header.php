2af6de766b077ff5f94862eb10387d921d0d0d5b
Gabriel THEUWS <Gabriel.THEUWS@etu.uca.fr>
2020-12-08 10:34:48 +0100

    [MODIF] du header.php, chemin rectifié

--- vue/header.php
+++ vue/header.php
@@ -11,7 +11,7 @@
   <header>
     <title>News automatisées par flux RSS</title>
     <nav class="border-bottom border-secondary">
-    	<a href="index.php"><img src="img/mailbox.png" width="36" height="36" alt="img/list.png" /></a> <!--Cliquer sur l'image fera revenir à la page d'articles-->
+    	<a href="index.php"><img src="vue/img/mailbox.png" width="36" height="36" alt="img/mailbox.png" /></a> <!--Cliquer sur l'image fera revenir à la page d'articles-->
     	<p class="navbar-text text-white">News automatisées par flux RSS</p>
       <ul class="nav justify-content-end">
         <li class="nav-item btn-link"><a href="controller/test.php" class="nav-link active text-white">page de test</a></li>
